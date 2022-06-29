dd1f18baf2f009a4faddbae8ae1d7a8dbf017a54
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-06-29 23:35:37 +0530

    Add project files.

---
+++ WasmServerPrerender/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WasmServerPrerender;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+if (!builder.RootComponents.Any())
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
+
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
+
+await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+}
